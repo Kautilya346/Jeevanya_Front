82e62b5a3adc9f689c8c90421c333f6d348d9a19
Aditya Karanwal <23ucs514@lnmiit.ac.in>
2025-03-08 02:43:56 +0530

    user_profile

--- Jeevanya_Front/src/App.jsx
+++ Jeevanya_Front/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-
+import UserProfile from "./pages/UserProfile";
 import NotFound from "./pages/NotFound";
 import AIDiagnose from "./pages/AI_Diagonise";
 import Login from "./pages/Login";
@@ -15,6 +14,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/aidiagnose" element={<AIDiagnose />} />
+        <Route path="/UserProfile" element={<UserProfile />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>

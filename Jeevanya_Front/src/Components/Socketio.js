35f319e04860f671d84ac895647e404dfa079e17
Kautilya03 <23ucs613@gmail.com>
2025-03-09 01:10:52 +0530

    likeer

--- Jeevanya_Front/src/Components/Socketio.js
+++ Jeevanya_Front/src/Components/Socketio.js
@@ -1,5 +1,7 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000");
+const socket = io("http://localhost:3000", {
+    transports: ["websocket"], // Ensures WebSocket connection
+  });
 
 export default socket;
